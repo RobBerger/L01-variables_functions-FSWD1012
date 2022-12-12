7683f620632151acefa60c98c708b53d52c706b5
berger <robertberger.dev@gmail.com>
2022-12-12 13:19:23 -0500

    Added let and const variables

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // alert("It's working");
 
-var welcome =  "Hello there!"
-console.log(welcome);
+// var welcome =  "Hello there!"
+// console.log(welcome);
+
+// let greeting = "Hello, human!";
+// console.log(greeting);
+
+// let greeting = "Good Morning!";
+// console.log(greeting);
+
+let greeting = "Hello, human!";
+greeting = "Good Morning!";
+console.log(greeting);
+
+const constant = "I am a constant variable!";
+console.log(constant);
